fd1c4b9ea803f5ec8388e161416396165b2cda6c
DJ Wilson <thadeej@gmail.com>
2014-05-28 16:22:38 -0700

    Fixed PDF export in org-mode

--- init.el
+++ init.el
@@ -7,6 +7,21 @@
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
+;; Remedy PDF export issue
+;; pdflatex command not found
+;; This code should pull $PATH from user .bash_profile
+(defun set-exec-path-from-shell-PATH ()
+  "Sets the exec-path to the same value used by the user shell"
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+;; End PDF export code
+
 (zconfig-start-benchmark)
 
 (zconfig-load "fountain-mode")
