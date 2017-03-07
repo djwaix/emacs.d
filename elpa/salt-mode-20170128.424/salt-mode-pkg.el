b4f14b874fd1aca9c749b65deb419564d0d44fca
DJ Wilson <thadeej@gmail.com>
2017-03-07 15:50:41 -0500

    /elpa packages

---
+++ elpa/salt-mode-20170128.424/salt-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "salt-mode" "20170128.424" "Major mode for Salt States" '((yaml-mode "0.0.12") (mmm-mode "0.5.4") (mmm-jinja2 "0.0.1")) :url "https://github.com/glynnforrest/salt-mode" :keywords '("languages"))
